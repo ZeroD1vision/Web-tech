d906868c0fdcfa023fa227c49f0531900f2dc6f9
zerodiv <narz.artem@gmail.com>
2025-03-19 21:11:37 +0300

    Написал клиент и сервер 1.0

--- ignored/client-pizdec.cpp
+++ ignored/client-pizdec.cpp
@@ -4,7 +4,7 @@
 #include <sstream>
 #include <ctime>
 #ifdef __GNUC__
-    #define _WIN32_WINNT 0x600
+    #define _WIN32_WINNT 0x0501
 #endif
 #include <winsock2.h>
 #include <ws2tcpip.h>
@@ -12,11 +12,45 @@
 #pragma comment(lib, "ws2_32.lib") // Подключение библиотеки Winsock
 
 
+class Log {
+    public:
+        std::string timeConn;
+        std::string timeRecv;
+        std::string timeSend;
+        std::string timeClose;
+        std::string addr;
+        std::string dataRecv;
+        std::string dataSend;
+    
+        void PrintLog() const {
+            std::ofstream logFile("client_log.txt", std::ios_base::app);
+            if (logFile.is_open()) {
+                logFile << "Адрес сервера:       "      << addr     << '\n';
+                logFile << "Подключение к серверу: "    << timeConn << '\n';
+                logFile << "Отправлено сообщение: "     << timeSend << '\n';
+                logFile << "Отправленное сообщение:\n " << dataSend << '\n';
+                logFile << "Получение ответа:    "      << timeRecv << '\n';
+                logFile << "Полученное сообщение:\n"    << dataRecv << '\n';
+                logFile << "Завершение сеанса:   "      << timeClose << "\n\n\n\n";
+                logFile.close();
+            }
+        }
+    };
+
+
+std::string getCurrentTime() {
+    time_t now = time(0);
+    std::string timeStr = ctime(&now);
+    return timeStr.substr(0, timeStr.length() - 1); // Удаляем символ новой строки
+}
+
+
 /**
  * Функция для записи события в лог-файл.
  *
  * @param message Сообщение, которое будет записано в лог.
  */
+/***************************************************************
 void logEvent(const std::string& message) {
     std::ofstream logFile("client_log.txt", std::ios_base::app);
     if (logFile.is_open()) {
@@ -26,8 +60,12 @@ void logEvent(const std::string& message) {
         logFile << dt << " - " << message << std::endl;
         logFile.close();
     }
-}
+}**************************************************************/
 
+void logEvent(Log& log, const std::string& message, const std::string& time) {
+    log.dataSend = message; // Устанавливаем отправляемое сообщение
+    log.timeSend = time;    // Устанавливаем время отправки
+}
 
 /**
  * Основная функция клиента.
@@ -38,31 +76,45 @@ void logEvent(const std::string& message) {
  * @return Код завершения программы.
  */
 int main() {
-    // Инициализация Winsock
     WSADATA wsaData;                                 // Структура для хранения версии Winsock и используемых протоколах
+    Log myLog;                                       // Создаем объект класса Log
+
+    // Инициализация Winsock
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) { // Используем версию 2.2 Winsock 
-        logEvent("Ошибка инициализации Winsock");
+        logEvent(myLog, "Ошибка инициализации Winsock", getCurrentTime());
         return 1;
     }
 
     // Загрузка конфигурации
-    std::string serverAddress = "127.0.0.1"; // По умолчанию
-    int serverPort = 65432;                  // По умолчанию
+    std::string serverAddress = "127.0.0.1"; // IP-адрес умолчанию
+    int serverPort = 65432;                  // Порт по умолчанию
 
     // Чтение конфигурации из файла
     std::ifstream configFile("config.txt");
     if (configFile.is_open()) {
-        configFile >> serverAddress >> serverPort;
+        std::string line;
+        while (std::getline(configFile, line)) {
+            std::istringstream configStream(line);
+            std::string key;
+            if (configStream >> key) {
+                if (key == "ip_address") {
+                    configStream >> serverAddress; // Чтение IP-адреса
+                } else if (key == "port") {
+                    configStream >> serverPort; // Чтение порта
+                }
+            }
+        }
         configFile.close();
     } else {
-        logEvent("Не удалось открыть файл конфигурации. Используются настройки по умолчанию.");
-    }
+        logEvent(myLog, "Не удалось открыть файл конфигурации. Используются настройки по умолчанию.", getCurrentTime());
+    }//куда мы запишем это. Как происходит запись в функцию
+
+    myLog.addr = serverAddress; // Устанавливаем адрес для лога
 
     // Создание сокета
     SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == INVALID_SOCKET) {
-        logEvent("Ошибка создания сокета: " + 
-            std::to_string(WSAGetLastError()));
+        logEvent(myLog, "Ошибка создания сокета: " + std::to_string(WSAGetLastError()), getCurrentTime());
         WSACleanup();
         return 1;
     }
@@ -70,17 +122,19 @@ int main() {
     sockaddr_in server;                                          // Структура, содержит информацию о сервере, к которому присоединяемся    
     server.sin_family = AF_INET;                                 // Указываем, что используем IPv4
     server.sin_port = htons(serverPort);                         // Преобразуем порт в сетевой порядок байтов
-    //inet_pton(AF_INET, serverAddress.c_str(), &server.sin_addr); // Преобразуем строку IP-адреса в формат, используемый в sockaddr_in
-    server.sin_port = htons(serverPort); // Преобразуем порт в сетевой порядок байтов
-    
-    // Использование inet_addr вместо inet_pton
-    server.sin_addr.s_addr = inet_addr(serverAddress.c_str());
+    server.sin_addr.s_addr = inet_addr(serverAddress.c_str());   // Преобразуем строку IP-адреса в формат, используемый в sockaddr_in
+    //inet_pton(AF_INET, serverAddress.c_str(), &server.sin_addr);
+
     if (server.sin_addr.s_addr == INADDR_NONE) {
-        logEvent("Некорректный IP-адрес: " + serverAddress);
+        logEvent(myLog, "Некорректный IP-адрес: " + serverAddress, getCurrentTime());
         closesocket(sock);
         WSACleanup();
         return 1;
     }
+
+    // Устанавливаем время подключения
+    myLog.timeConn = getCurrentTime();
+
     /**
     * Подключение к серверу.
     *  
@@ -88,14 +142,16 @@ int main() {
     * записывается сообщение об ошибке, сокет закрывается, и программа завершает выполнение.
     */
     if (connect(sock, (struct sockaddr*)&server, sizeof(server)) == SOCKET_ERROR) {
-        logEvent("Ошибка подключения к серверу " + serverAddress + ":" + 
-            std::to_string(serverPort) + " - " + std::to_string(WSAGetLastError()));
+        logEvent(myLog, "Ошибка подключения к серверу: " + 
+                 std::to_string(WSAGetLastError()), getCurrentTime());
         closesocket(sock);
         WSACleanup();
         return 1;
     }
-    logEvent("Подключение к серверу " + serverAddress + ":" + std::to_string(serverPort));
 
+    // Логируем успешное подключение
+    logEvent(myLog, "Подключение к серверу " + serverAddress + ":" + std::to_string(serverPort), getCurrentTime());
+    
     // Формирование сообщения
     std::string fio = "Нарзиев Артемий Тимурович";
     std::string group = "М3О-119БВ-24";
@@ -104,8 +160,12 @@ int main() {
     // Задержка перед отправкой
     Sleep(2000);
 
+    // Устанавливаем время отправки и сохраняем сообщение
+    myLog.timeSend = getCurrentTime();
+    myLog.dataSend = message;
+
     // Отправка сообщения
-    logEvent("Отправка сообщения: " + message);
+    logEvent(myLog, "Отправка сообщения: " + message, getCurrentTime());
     int bytesSent = send(sock, message.c_str(), message.length(), 0);
     if (bytesSent == SOCKET_ERROR) {
         logEvent("Ошибка отправки сообщения: " + std::to_string(WSAGetLastError()));
@@ -113,6 +173,9 @@ int main() {
         WSACleanup();
         return 1;
     }
+
+    // Выводим время отправки и сообщение
+    logEvent(myLog, "Сообщение отправлено: " + message, myLog.timeSend);
 
     /**
     * Ожидание ответа от сервера и обработка полученных данных.
@@ -125,8 +188,10 @@ int main() {
     int bytesReceived = recv(sock, buffer, sizeof(buffer) - 1, 0);
     
     if (bytesReceived > 0) {
-        buffer[bytesReceived] = '\0'; // Завершение строки
-        logEvent("Получено сообщение от сервера: " + std::string(buffer));
+        buffer[bytesReceived] = '\0';
+        myLog.dataRecv = std::string(buffer, bytesReceived);
+        myLog.timeRecv = getCurrentTime(); // Устанавливаем время получения
+        logEvent(myLog, "Сообщение получено: " + myLog.dataRecv, myLog.timeRecv);
     } else if (bytesReceived == SOCKET_ERROR) {
         logEvent("Ошибка получения сообщения от сервера: " + std::to_string(WSAGetLastError()));
     } else {
