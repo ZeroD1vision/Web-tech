ecf4e8fd2cc7ce6b71046755a524dca88f96d4d0
zerodiv <narz.artem@gmail.com>
2025-04-14 18:31:09 +0300

    .gitignore

--- Labs/Lab4/Codes/Express/models/userModel.js
+++ Labs/Lab4/Codes/Express/models/userModel.js
@@ -1,18 +1,23 @@
 const users = []; // Хранилище пользователей
 
-module.exports = users;
-
 // Функция для добавления пользователя
 exports.addUser  = (user) => {
     users.push(user);
 };
 
 // Функция для поиска пользователя по имени
-exports.find_user_by_username = (username) => {
+exports.findUserByUsername = (username) => {
     return users.find(user => user.username === username);
+};
+
+// Функция для проверки существования никнейма
+exports.nicknameExists = (nickname) => {
+    return users.some(user => user.nickname === nickname);
 };
 
 // Функция для получения всех пользователей (если потребуется)
 exports.getAllUsers = () => {
     return users;
 };
+
+module.exports = { users, addUser , findUserByUsername, nicknameExists, getAllUsers };
