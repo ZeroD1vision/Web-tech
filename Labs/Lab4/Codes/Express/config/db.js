8b4d2fce23a3ce31f7d3a43d3371fd3133fec78b
zerodiv <narz.artem@gmail.com>
2025-04-14 18:35:30 +0300

    .gitignore

---
+++ Labs/Lab4/Codes/Express/config/db.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { Client } = require('pg');
+
+const client = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+});
+
+client.connect()
+    .then(() => console.log('Подключение к PostgreSQL успешно!'))
+    .catch(err => console.error('Ошибка подключения к PostgreSQL', err.stack));
+    
+module.exports = client;
