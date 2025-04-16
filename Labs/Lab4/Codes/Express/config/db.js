fa179779fd44bba6876a81331238ab293ec5e9b4
zerodiv <narz.artem@gmail.com>
2025-04-16 06:00:34 +0300

    База данных с таблицей фильмов

--- Labs/Lab4/Codes/Express/config/db.js
+++ Labs/Lab4/Codes/Express/config/db.js
@@ -1,7 +1,9 @@
-const { Client } = require('pg');
+//const { Client } = require('pg');
+const { Pool } = require('pg');
 require('dotenv').config();
 
-const client = new Client({
+// Создаем пул соединений с базой данных
+const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
@@ -9,8 +11,26 @@ const client = new Client({
     port: process.env.DB_PORT,
 });
 
-client.connect()
+// Проверяем соединение с базой данных
+pool.connect()
     .then(() => console.log('Подключение к PostgreSQL успешно!'))
     .catch(err => console.error('Ошибка подключения к PostgreSQL', err.stack));
-    
-module.exports = client;
+
+
+// Функция для получения всех фильмов
+const getAllMovies = async () => {
+    const res = await pool.query('SELECT * FROM movies');
+    return res.rows;
+};
+
+// Функция для получения фильма по ID
+const getMovieById = async (id) => {
+    const res = await pool.query('SELECT * FROM movies WHERE id = \$1', [id]);
+    return res.rows[0];
+};
+
+module.exports = {
+    getAllMovies,
+    getMovieById,
+    pool,
+};
