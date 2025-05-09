2519dd9d510ccad1768d6ebd7bfa549d0d0fcdfd
zerodiv <narz.artem@gmail.com>
2025-05-09 19:30:39 +0300

    Неуверенная версия с Аутентификацией с Cookie

--- Labs/coursework/Codes/Client/src/api/axiosInstance.js
+++ Labs/coursework/Codes/Client/src/api/axiosInstance.js
@@ -5,35 +5,46 @@ const instance = axios.create({
   withCredentials: true
 });
 
-instance.interceptors.request.use(config => {
-  const token = localStorage.getItem('accessToken');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+// Убираем перехватчик для добавления токена в заголовки
+
+// instance.interceptors.request.use(config => {
+//   const token = localStorage.getItem('accessToken');
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
 
 instance.interceptors.response.use(
   response => response,
   async error => {
     const originalRequest = error.config;
     
+    // Обрабатываем код 419 (просроченный токен)
     if (error.response?.status === 419 && !originalRequest._retry) {
       originalRequest._retry = true;
       
       try {
-        const refreshToken = localStorage.getItem('refreshToken');
-        const response = await axios.post('/api/auth/refresh', { refreshToken });
+        // Отправляем запрос на обновление токенов
+        const response = await instance.post(
+          '/api/auth/refresh', 
+          {}, 
+          { withCredentials: true }
+        );
+        // const refreshToken = localStorage.getItem('refreshToken');
+        // const response = await axios.post('/api/auth/refresh', { refreshToken });
         
-        localStorage.setItem('accessToken', response.data.accessToken);
-        localStorage.setItem('refreshToken', response.data.refreshToken);
+        // localStorage.setItem('accessToken', response.data.accessToken);
+        // localStorage.setItem('refreshToken', response.data.refreshToken);
 
-        originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`;
+        // originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`;
         return instance(originalRequest);
       } catch (refreshError) {
-        localStorage.removeItem('accessToken');
-        localStorage.removeItem('refreshToken');
+        // localStorage.removeItem('accessToken');
+        // localStorage.removeItem('refreshToken');
+        // Перенаправляем на логин при ошибке обновления
         window.location.href = '/login';
+        return Promise.reject(refreshError);
       }
     }
     
